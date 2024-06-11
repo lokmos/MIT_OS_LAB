3d2f45fc38f3b86fcd90b62c1a5b524136f224c3
lokmos <23110240153@m.fudan.edu.cn>
2024-06-11 05:00:42 -0700

    Lab1 All Tasks Finished

--- user/find.c
+++ user/find.c
@@ -4,7 +4,7 @@
 #include "kernel/fs.h"
 
 void
-ls(char *path)
+find(char *path, char *target)
 {
   char buf[512], *p;
   int fd;
@@ -12,24 +12,25 @@ ls(char *path)
   struct stat st;
 
   if((fd = open(path, 0)) < 0){
-    fprintf(2, "ls: cannot open %s\n", path);
+    fprintf(2, "find: cannot open %s\n", path);
     return;
   }
 
   if(fstat(fd, &st) < 0){
-    fprintf(2, "ls: cannot stat %s\n", path);
+    fprintf(2, "find: cannot stat %s\n", path);
     close(fd);
     return;
   }
 
   switch(st.type){
   case T_FILE:
-    printf("%s %d %d %l\n", fmtname(path), st.type, st.ino, st.size);
+    if (strcmp(path + strlen(path) - strlen(target), target) == 0)
+        printf("%s\n", path);
     break;
 
   case T_DIR:
     if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-      printf("ls: path too long\n");
+      printf("find: path too long\n");
       break;
     }
     strcpy(buf, path);
@@ -41,10 +42,11 @@ ls(char *path)
       memmove(p, de.name, DIRSIZ);
       p[DIRSIZ] = 0;
       if(stat(buf, &st) < 0){
-        printf("ls: cannot stat %s\n", buf);
+        printf("find: cannot stat %s\n", buf);
         continue;
       }
-      printf("%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+      if (strcmp(buf + strlen(buf) - 2, "/.") != 0 && strcmp(buf + strlen(buf) - 3, "/..") != 0)
+        find(buf, target);
     }
     break;
   }
@@ -54,13 +56,12 @@ ls(char *path)
 int
 main(int argc, char *argv[])
 {
-  int i;
-
-  if(argc < 2){
-    ls(".");
+  if(argc < 3){
     exit(0);
   }
-  for(i=1; i<argc; i++)
-    ls(argv[i]);
-  exit(0);
+    char target[512];
+	target[0] = '/'; // 为查找的文件名添加 / 在开头
+	strcpy(target+1, argv[2]);
+	find(argv[1], target);
+	exit(0);
 }
